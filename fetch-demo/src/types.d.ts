d29c91fb3e0957dc87a12b86266e8bb6fff9a727
Pranav Manjunath <38512462+pmanjunath29@users.noreply.github.com>
2023-03-29 22:58:16 -0400

    Update types.d.ts

--- fetch-demo/src/types.d.ts
+++ fetch-demo/src/types.d.ts
@@ -33,14 +33,26 @@ interface Seed {
 interface Track {
     album: Album;
     artists: Artist[];
+    available_markets: string[]
     disc_number: number;
     duration_ms: number;
     explicit: boolean;
-    external_ids: ExternalID;
-    external_urls: ExternalURL;
+    external_ids: {
+        isrc: string;
+        ean: string;
+        upc: string;
+    };
+    external_urls: {
+        spotify: string;
+    };
     href: string;
     id: string;
     is_playable: boolean;
+    linked_from: {
+    };
+    restrictions: {
+        reason: string;
+    }
     name: string;
     popularity: number;
     preview_url: string;
@@ -53,38 +65,50 @@ interface Track {
 interface Album {
     album_type: string;
     total_tracks: number;
-    external_urls: ExternalURL;
+    available_markets: string[];
+    external_urls: {
+        spotify: string;
+    };
     href: string;
     id: string;
     images: Image[]
     name: string;
     release_date: string;
     release_date_precision: string;
+    restrictions: {
+        reason: string;
+    }
     type: string;
     uri: string;
+    copyrights: Copyright[]
+    external_ids: {
+        isrc: string;
+        ean: string;
+        upc: string;
+    };
+    genres: string[];
+    label: string;
+    popularity: number;
     album_group: string;
     artists: Artist[];
-    is_playable: boolean
 }
 interface Artist {
-    external_urls: ExternalURL
-    href: string
-    id: string
-    name: string
-    type: string
-    uri: string
+    external_urls: {
+        spotify: string;
+    };
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
 }
-
-interface ExternalURL {
-    spotify: string
-}
-
-interface ExternalID {
-    isrc: string
-}
-
 interface Image {
     url: string;
     height: number;
     width: number;
+}
+
+interface Copyright {
+    text: string;
+    type: string;
 }
